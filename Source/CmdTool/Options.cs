6e8deb825190f613f04e7a55bfb8dee8ee1e25a3
Miguel Sotolongo <miso@microsoft.com>
2016-10-25 17:23:42 -0700

    Experimenting with c# attributes

---
+++ Source/CmdTool/Options.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CmdTool
+{
+    public class Command : Attribute
+    {
+        public string Command { get; private set; }
+
+        public Command(string command)
+        {
+            Command = command;
+        }
+    }
+
+    public class Option : Attribute
+    {
+        public string Command { get; private set; }
+        public string Description { get; private set; }
+        public bool Required { get; private set; }
+
+        public Option(string command, string description, bool required)
+        {
+            Command = command;
+            Description = description;
+            Required = required;
+        }
+    }
+}
---
+++ Source/CmdTool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using CmdTool;
+
+namespace CmdTool
+{
+    [Command("test")]
+    [Description("This is a simple test class with some arguments.")]
+    class TestCommand
+    {
+        [Option("-v", "Verbose flag", true)]
+        private const bool Verbose = false;
+
+        public void Run()
+        {
+            Console.WriteLine(Verbose);
+        }
+    }
+
+    /// <summary>
+    /// Collection of command line scripts.
+    /// </summary>
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments found, use -help for instructions.");
+                return 0;
+            }
+
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
